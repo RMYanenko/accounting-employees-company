66581fa762e90f9a4511cab9c6da0170bc439bd1
RMYanenko <yanenko.r.m@gmail.com>
2021-10-01 16:47:23 +0300

    change class item

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,28 +1,37 @@
-import './employees-list-item.css';
-import cn from 'classnames';
+import "./employees-list-item.css";
+import cn from "classnames";
 
-const EmployeesListItem = ({name, salary, bonus}) => {
+const EmployeesListItem = ({ name, salary, bonus }) => {
+  return (
+    <li
+      className={cn(
+        "list-group-item",
+        " d-flex",
+        "justify-content-between",
+        "className",
+        {
+          increase: bonus === true,
+        }
+      )}
+    >
+      <span className="list-group-item-label">{name}</span>
+      <input
+        type="text"
+        className="list-group-item-input"
+        defaultValue={salary + "$"}
+      />
+      <div className="d-flex justify-content-center align-items-center">
+        <button type="button" className="btn-cookie btn-sm ">
+          <i className="fas fa-cookie"></i>
+        </button>
 
-    return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className={cn('btn-sm', 'className', {
-                        'btn-cookie': bonus === true
-                    })}>
-                    <i className="fas fa-cookie"></i>
-                </button>
-
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
-}
+        <button type="button" className="btn-trash btn-sm ">
+          <i className="fas fa-trash"></i>
+        </button>
+        <i className="fas fa-star"></i>
+      </div>
+    </li>
+  );
+};
 
 export default EmployeesListItem;
